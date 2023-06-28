13e88a87fc6143eba8b43a102a4bb2b2012b334c
polandru <paulperdomo@comcast.net>
2023-06-28 17:18:23 -0500

    Assignment 6 pat 1 - First attempt

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,20 +10,37 @@
 #include <fcntl.h>
 #include <syslog.h>
 #include <arpa/inet.h>
+#include "queue.h"
+#include <pthread.h>
+#include <stdbool.h>
+#include <time.h>
 #define BUFFSIZE 25000
+#define _XOPEN_SOURCE 700
 
-int sockfd, connfd;
+typedef struct slist_data_s slist_data_t;
+struct slist_data_s
+{
+	pthread_t thread;
+	bool running;
+	int connfd;
+	SLIST_ENTRY(slist_data_s)
+	entries;
+};
+
+int sockfd;
+pthread_mutex_t write_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t timestamp_thread;
+struct sigaction sa;
+
 void handle_signals(int sgno)
 {
-	if (sgno == SIGTERM || sgno == SIGINT)
-	{
-		close(connfd);
-		close(sockfd);
-		chdir("/var/tmp");
-		remove("aesdsocketdata");
-		syslog(LOG_ALERT,"Caught signal exiting");
-		exit(0);
-	}
+
+	close(sockfd);
+	pthread_cancel(timestamp_thread);
+	chdir("/var/tmp");
+	remove("aesdsocketdata");
+	syslog(LOG_ALERT, "Caught signal exiting");
+	exit(0);
 }
 
 int send_data(int connfd, FILE *fp)
@@ -37,32 +54,21 @@ int send_data(int connfd, FILE *fp)
 	free(data);
 	return 0;
 }
-void handle_connection(int connfd)
+void *handle_connection(void *d)
 {
+	slist_data_t *data = (slist_data_t *)d;
+	int connfd = data->connfd;
+	data->running = true;
+
 	char *buff;
 	char tmp_buff[BUFFSIZE];
 
 	for (;;)
 	{
 
-
 		buff = calloc(BUFFSIZE + 1, sizeof(char));
 		int msg_len;
-		int read_count = 1;
-		while ((msg_len = read(connfd, tmp_buff, BUFFSIZE)) >= BUFFSIZE)
-		{
-			
-			strcat(buff, tmp_buff);
-			char * t;
-			t = realloc(buff, ++read_count * BUFFSIZE + 1);
-			if(t){
-				buff = t;
-			}else{
-				syslog(LOG_ALERT, "REALLOC ERROR" );
-			}
-			syslog(LOG_ALERT, "REALLOC SIZE: %d",read_count * BUFFSIZE + 1 );
-		}
-
+		msg_len = read(connfd, tmp_buff, BUFFSIZE);
 		if (msg_len == 0)
 		{
 			break;
@@ -72,25 +78,50 @@ void handle_connection(int connfd)
 		strcat(buff, tmp_buff);
 
 		FILE *output_file = fopen("/var/tmp/aesdsocketdata", "at+");
-
+		pthread_mutex_lock(&write_mutex);
 		fprintf(output_file, "%s", buff);
 		fflush(output_file);
+		pthread_mutex_unlock(&write_mutex);
 		send_data(connfd, output_file);
 
 		free(buff);
 		fclose(output_file);
 	}
+
 	close(connfd);
 	free(buff);
+	return NULL;
+}
+
+void *write_time(void *a)
+{
+
+	while (1)
+	{
+		char time_str[50];
+		time_t raw;
+		time(&raw);
+		strftime(time_str, 50, "%D %T", localtime(&raw));
+		FILE *output_file = fopen("/var/tmp/aesdsocketdata", "at+");
+		pthread_mutex_lock(&write_mutex);
+		fprintf(output_file, "timestamp:%s\n", time_str);
+		fflush(output_file);
+		pthread_mutex_unlock(&write_mutex);
+		sleep(10);
+	}
 }
 
 int main(int c, char **argv)
 {
+	sa.sa_handler = handle_signals;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
+
+
 	int len;
 	struct sockaddr_in servaddr, cli;
-
-	signal(SIGINT, handle_signals);
-	signal(SIGTERM, handle_signals);
 
 	// socket create and verification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -112,7 +143,6 @@ int main(int c, char **argv)
 		perror("BIND Failed");
 		exit(-1);
 	}
-	
 
 	if (c == 2 && strcmp(argv[1], "-d") == 0)
 	{
@@ -138,33 +168,41 @@ int main(int c, char **argv)
 		/* stderror */
 	}
 
-	// Now server is ready to listen and verification
+	pthread_create(&timestamp_thread, NULL, write_time, NULL);
 	if ((listen(sockfd, 5)) != 0)
 	{
 		perror("listen failed");
 		exit(-1);
 	}
 	else
-	len = sizeof(cli);
+		len = sizeof(cli);
 
 	while (1)
 	{
 		char ip[INET_ADDRSTRLEN];
-		// Accept the data packet from client and verification
-		connfd = accept(sockfd, (struct sockaddr *)&cli, (socklen_t *)&len);
+
+		int connfd = accept(sockfd, (struct sockaddr *)&cli, (socklen_t *)&len);
 		if (connfd < 0)
 		{
 			perror("Accept failed");
 			exit(-1);
 		}
-		else{
+		else
+		{
 
-			inet_ntop(AF_INET,&(cli.sin_addr),ip,INET_ADDRSTRLEN);
-			syslog(LOG_INFO,"Accepted connection from %s",ip);
+			inet_ntop(AF_INET, &(cli.sin_addr), ip, INET_ADDRSTRLEN);
+			syslog(LOG_INFO, "Accepted connection from %s", ip);
 		}
 
-		// Function for chatting between client and server
-		handle_connection(connfd);
-		syslog(LOG_INFO,"Closed connection from %s",ip);
+		STAILQ_HEAD(stailqhead, stailq_data_s)
+		head;
+		STAILQ_INIT(&head);
+
+		slist_data_t *data = malloc(sizeof(slist_data_t));
+		data->connfd = connfd;
+
+		pthread_create(&data->thread, NULL, handle_connection, (void *)data);
+
+		syslog(LOG_INFO, "Closed connection from %s", ip);
 	}
 }
